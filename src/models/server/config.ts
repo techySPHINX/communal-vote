93290831b00b47d8b62bdc379918956cdfc3d4eb
techySPHINX <shimon123universe@gmail.com>
2024-07-25 09:49:44 +0530

    Database sorted

---
+++ src/models/server/config.ts
@@ -0,0 +1,20 @@
+import env from "@/app/env";
+
+import {Avatars, Client, Databases, Storage, Users} from "node-appwrite"
+
+let client = new Client();
+
+client
+    .setEndpoint(env.appwrite.endpoint) 
+    .setProject(env.appwrite.projectId) 
+    .setKey(env.appwrite.apikey) 
+    
+;
+
+const databases = new Databases(client)
+const avatars = new Avatars(client);
+const storage = new Storage(client);
+const users = new Users(client)
+
+
+export { client, databases, users, avatars, storage}
